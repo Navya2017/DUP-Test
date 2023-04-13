a57b5338dc8c019d0ddaac6c044ba0ac55581930
Navya <monaharisha2017@gmail.com>
2023-04-13 15:19:23 +0400

    committed

---
+++ src/test/java/sample/Demo2.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+
+	public void demo()
+	{
+		System.out.println("This is demo");
+		
+		System.out.println("This is correct");
+}
+}
